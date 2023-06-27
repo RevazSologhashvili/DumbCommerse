1862b4c546bfebe599a3166aeb628124a0047d9c
Me <revaz.sologhashvili@gmail.com>
2023-06-27 10:52:43 +0400

    Added Comments data to a new comments branch

--- components/Products/Products.tsx
+++ components/Products/Products.tsx
@@ -1,53 +1,20 @@
 'use client'
-import axios from "axios";
 import Styles from './Products.module.css'
-import {useState} from "react";
-let getOnce = true;
+import { data } from './data'
 
-type listProps = [
-    title: string,
-    image: string,
-    price: string,
-    id:    string,
-];
 
 function Products() {
 
-    const [products, setProducts] = useState([]);
-    const [sort, setSort] = useState('asc')
-
-    const sortChange = (e)=>{
-            if(e.target.value === 'Ascending') setSort('asc');
-            else if(e.target.value === 'Descending') setSort('desc');
-            getOnce = true;
-    }
-    if(getOnce === true) {
-        axios.get(`https://fakestoreapi.com/products?sort=${sort}`)
-            .then((result) => {
-                setProducts(result.data);
-            })
-        getOnce = false;
-    }
-
-
+   
     return(
     <>
         <div className={Styles.wrapper}>
-            <select onChange={sortChange} className={Styles.sort}>
-                <option selected disabled>Sort Style</option>
-                <option>Ascending</option>
-                <option>Descending</option>
-            </select>
-
-            <ul className={Styles.flexList}>
-                {products.map((product: listProps) => (
-                    <li className={Styles.flexItem} key={product.id}>
-                        <span className={Styles.title}>{product.title}</span>
-                        <div
-                            className={Styles.imageContainer}
-                            style={{ backgroundImage: `url(${product.image})` }}
-                        ></div>
-                        <span className={Styles.price}>{`\$${product.price}`}</span>
+             <ul className={Styles.flexList}>
+                {data.map((datat: any) => (
+                    <li className={Styles.flexItem} key={datat.id}>
+                        <span className={Styles.price}>{datat.id}</span>
+                        <span className={Styles.title}>{datat.message}</span>
+                        <span className={Styles.price}>{datat.created_time}</span>
                     </li>
                 ))}
             </ul>
